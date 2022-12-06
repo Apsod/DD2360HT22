2061218f4f2a96d03e301ed817dbc99388006876
Amaru Cuba Gyllensten <amarucuba@gmail.com>
2022-12-06 15:25:29 +0000

    fixed stuff

--- hw_3/ex_1/lab3_ex1_template.cu
+++ hw_3/ex_1/lab3_ex1_template.cu
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <random>
+#include <vector>
 
 #define DataType double
 
@@ -11,11 +12,21 @@ __global__ void vecAdd(DataType *in1, DataType *in2, DataType *out, int len) {
   out[i] = in1[i] + in2[i];
 }
 
+
 //@@ Insert code to implement timer start
+
+clock_t CLOCK;
+
+void start_clock() {
+    CLOCK = clock();
+}
 
 //@@ Insert code to implement timer stop
 
-
+double stop_clock() {
+    clock_t stop_time = clock();
+    return ((double) (stop_time - CLOCK)) / CLOCKS_PER_SEC;
+}
 
 __host__ int asMultipleOf(int value, int factor) {
     // Returns the smallest value larger or equal to 
@@ -29,6 +40,7 @@ int main(int argc, char **argv) {
   
   int inputLength;
   int deviceLength;
+  double elapsed; 
   DataType *hostInput1;
   DataType *hostInput2;
   DataType *hostOutput;
@@ -37,7 +49,6 @@ int main(int argc, char **argv) {
   DataType *deviceInput2;
   DataType *deviceOutput;
   
-
   // Initialize the 1D grid and block dimensions here
 
   // Read in inputLength from args
@@ -46,12 +57,12 @@ int main(int argc, char **argv) {
 
   int TPB = 32;
 
-  deviceLength = 132; //asMultipleOf(inputLength, TPB);
+  deviceLength = asMultipleOf(inputLength, TPB);
 
   int blocks = deviceLength / TPB;
 
-  printf("The input length is %d\n", inputLength);
-  printf("The device length is %d\n", deviceLength);
+  printf("The input length is   : %d\n", inputLength);
+  printf("The device length is  : %d\n", deviceLength);
   
   // Allocate Host memory for input and output
   hostInput1 = (DataType*) malloc(inputLength * sizeof *hostInput1);
@@ -70,54 +81,86 @@ int main(int argc, char **argv) {
           ptr[i] = distribution(gen);
       }
   }
-
+  
+  start_clock();
   // Create reference result in CPU
   for (int i=0; i < inputLength; ++i){
       resultRef[i] = hostInput1[i] + hostInput2[i];
   }
+  elapsed = stop_clock();
+  printf("host execution time   : %f\n", elapsed);
+
 
   // Allocate GPU memory
-
+  
+  start_clock();
   cudaMalloc(&deviceInput1, deviceLength * sizeof*deviceInput1);
   cudaMalloc(&deviceInput2, deviceLength * sizeof*deviceInput2);
   cudaMalloc(&deviceOutput, deviceLength * sizeof*deviceOutput);
+  elapsed = stop_clock();
+  printf("cudamalloc            : %f\n", elapsed);
 
 
   // Copy memory to the GPU
-
+  
+  start_clock();
   cudaMemcpy(deviceInput1, hostInput1, inputLength * sizeof *hostInput1, cudaMemcpyHostToDevice);
   cudaMemcpy(deviceInput2, hostInput2, inputLength * sizeof *hostInput2, cudaMemcpyHostToDevice);
+  elapsed = stop_clock();
+  printf("host to device memcpy : %f\n", elapsed);
 
 
 
   //@@ Launch the GPU Kernel here
-    
+  start_clock();
   vecAdd<<<blocks, TPB>>>(deviceInput1, deviceInput2, deviceOutput, inputLength);
+  elapsed = stop_clock();
+  printf("kernel execution time : %f\n", elapsed);
 
   //@@ Copy the GPU memory back to the CPU here
 
+  start_clock();
   cudaMemcpy(hostOutput, deviceOutput, inputLength * sizeof*hostOutput, cudaMemcpyDeviceToHost);
-
+  elapsed = stop_clock();
+  printf("device to host memcpy : %f\n", elapsed); 
 
   //@@ Insert code below to compare the output with the reference
 
-  double delta = 0.0;
+  DataType m = hostOutput[0] - resultRef[0];
+  DataType m2 = 0.0;
 
-  for (int i=0; i<inputLength; ++i){
-    delta += pow(hostOutput[i] - resultRef[i], 2);
+  DataType maximumError = 0.0;
+
+  for (int i=1.0; i<inputLength; ++i) {
+    DataType delta;
+    DataType weight;
+    DataType diff;
+
+    weight = DataType(i);
+    diff = hostOutput[i] - resultRef[i];
+
+    delta = diff - m;
+
+    m += delta / (weight + 1.0);
+    m2 += pow(delta, 2) * weight / (weight + 1.0);
+    maximumError = max(abs(diff), maximumError);
   }
 
-  printf("euclidean distance between ref and out: %f\n", delta);
+  m2 = sqrt(m2 / DataType(inputLength-1));
 
-  //@@ Free the GPU memory here
+  printf("mean difference       : %f\n", m);
+  printf("standard deviation    : %f\n", m2);
+  printf("maximum error         : %f\n", maximumError);
+  
+  // Free the GPU memory
 
   for (DataType *ptr : {deviceInput1, deviceInput2, deviceOutput}) {
     cudaFree(ptr);
   }
 
-  //@@ Free the CPU memory here
+  // Free the CPU memory here
 
-  for (DataType *ptr : {hostInput1, hostInput2, hostOutput}) {
+  for (DataType *ptr : {hostInput1, hostInput2, hostOutput, resultRef}) {
     free(ptr);
   }
   return 0;
